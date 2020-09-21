ae9a0f1992b3062d1862899a125127463e67d5b4
BakaMondai <reppersonwo@gmail.com>
2020-09-20 20:48:25 -0500

    binary attempt one

---
+++ Binary.py
@@ -0,0 +1,42 @@
+import sys
+from textwrap import wrap
+
+def main():
+    content = getContent()
+    binaryConvert(content, 7)
+    binaryConvert(content, 8)
+
+
+def getContent():
+    for line in sys.stdin:
+        return line.rstrip()
+
+    
+def binaryConvert(content, length):
+    text = ""
+
+    if (len(content) % length) == 0: 
+
+        wrapped_content = wrap(content, length)
+
+        # print(wrapped_content)
+
+        for character in wrapped_content:
+
+            int_content = int(character, 2)
+            byte_number = int_content.bit_length() + 6
+            binary_array = int_content.to_bytes(byte_number, "big")
+            ascii_text = binary_array.decode().strip().strip('\x00')
+            text = text + ascii_text
+
+            # print(character)
+            # print(int_content)
+            # print(byte_number)
+            # print(binary_array)
+            # print(ascii_text)   
+
+        print("=============================================")
+        print("Solution : " + text);
+        print("=============================================")
+    
+main()
